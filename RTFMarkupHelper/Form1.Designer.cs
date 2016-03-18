0f76ae4ca0c8ad8b4f8481a640abfef3e5e9201b
Chris Emerson <chris.emerson@ymail.com>
2016-03-18 06:51:37 -0400

    20160318.01
    Initial push to GitHub 20160318.01

---
+++ RTFMarkupHelper/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace RTFMarkupHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rtfBox = new System.Windows.Forms.RichTextBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.textSpecials = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtfBox
+            // 
+            this.rtfBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtfBox.Location = new System.Drawing.Point(21, 49);
+            this.rtfBox.Name = "rtfBox";
+            this.rtfBox.Size = new System.Drawing.Size(398, 419);
+            this.rtfBox.TabIndex = 0;
+            this.rtfBox.Text = "";
+            this.rtfBox.TextChanged += new System.EventHandler(this.rtfBox_TextChanged);
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox.Location = new System.Drawing.Point(446, 49);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(311, 376);
+            this.textBox.TabIndex = 1;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Paste/Edit Rich Text Here:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(442, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "RTF Markup Code";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVersion.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblVersion.Location = new System.Drawing.Point(515, 535);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(242, 13);
+            this.lblVersion.TabIndex = 5;
+            this.lblVersion.Text = "RTF Markup Helper | Build: #DateWillShowHere#";
+            this.lblVersion.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.lblVersion.Click += new System.EventHandler(this.lblVersion_Click);
+            // 
+            // textSpecials
+            // 
+            this.textSpecials.BackColor = System.Drawing.SystemColors.Control;
+            this.textSpecials.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textSpecials.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSpecials.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textSpecials.Location = new System.Drawing.Point(153, 524);
+            this.textSpecials.Name = "textSpecials";
+            this.textSpecials.Size = new System.Drawing.Size(74, 19);
+            this.textSpecials.TabIndex = 6;
+            this.textSpecials.Text = "...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(18, 524);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Special Characters:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(502, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 66);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Copy to Clipboard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(446, 431);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 66);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "<<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(726, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(31, 28);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button4.Location = new System.Drawing.Point(320, 474);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Example Text";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // button5
+            // 
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button5.Location = new System.Drawing.Point(21, 474);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Clear Text";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textSpecials);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.rtfBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RTF Markup Helper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtfBox;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.TextBox textSpecials;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ RTFMarkupHelper/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RTFMarkupHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            string w_file = "RTFMarkupHelper.exe";
+            string w_directory = System.IO.Directory.GetCurrentDirectory();
+
+            DateTime c3 = System.IO.File.GetLastWriteTime(System.IO.Path.Combine(w_directory, w_file));            
+            lblVersion.Text = "RTF Markup Helper | Build: " + c3.ToString("yyyyMMdd") + "." + c3.ToString("HHmmss");
+
+            textSpecials.Text = "® © ™";
+        }
+
+        private void rtfBox_TextChanged(object sender, EventArgs e)
+        {
+            textBox.Text = rtfBox.Rtf.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblVersion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {            
+            DataFormats.Format myFormat = DataFormats.GetFormat(DataFormats.Html);
+
+            if (rtfBox.CanPaste(myFormat))
+            {
+                rtfBox.Paste(myFormat);         
+            }
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            rtfBox.Rtf = textBox.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("RTF Markup Helper is a simple C# app to make it easier to view, edit and convert RTF markup code.");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // default/example RTF (direct from SLO project 20160317.01)
+            rtfBox.Rtf = @"{\rtf1\ansi\ansicpg1252\deff0{\fonttbl{\f0\fswiss\fprq2\fcharset0 Franklin Gothic Book;}{\f1\froman\fprq2\fcharset0 Times New Roman;}{\f2\fnil Times New Roman;}}\viewkind4\uc1\pard\sl276\slmult1\lang1033\b\f0\fs22 D\'e9saccords\f1\par\b0\f0 Un d\'e9saccord est le terme utilis\'e9 lorsque le style de leadership que vous avez choisi pour vous-m\'eame ne r\'e9pond pas aux besoins de performance de la personne que vous influencez.\par\par\pard\sa200\sl276\slmult1 Plus le style de leadership est \'e9loign\'e9 du niveau de Performance Readiness\'ae (Maturit\'e9 (dans la performance)) du collaborateur qui est influenc\'e9, moins l\rquote interaction a de chances de r\'e9ussir. Ce ph\'e9nom\'e8ne peut \'e9galement \'eatre d\'e9fini comme le sur-dirigisme ou le sous-dirigisme.\par\pard\sl276\slmult1\b Sur-dirigisme\par\b0 Nous d\'e9finissons le sur-dirigisme comme l\rquote utilisation d\rquote un style de leadership actif o\'f9 le niveau de Performance Readiness\'ae (Maturit\'e9 (dans la performance)) de l\rquote individu qui est influenc\'e9 n\'e9cessite une implication moindre du leader.\f2\fs24\par}";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox.Text = rtfBox.Text = "";
+        }
+    }
+}
