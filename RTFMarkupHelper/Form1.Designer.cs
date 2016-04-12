c82492eb336ae77494cc8cd108a6e3490f123cd5
Chris Emerson <chris.emerson@ymail.com>
2016-04-12 10:57:43 -0400

    CE 20160412105714: added some basic formatting controls, gave up (again) on decode/encode asian characters for now

--- RTFMarkupHelper/Form1.Designer.cs
+++ RTFMarkupHelper/Form1.Designer.cs
@@ -35,7 +35,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.lblVersion = new System.Windows.Forms.Label();
-            this.textSpecials = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -43,6 +42,16 @@
             this.button4 = new System.Windows.Forms.Button();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.formatBold = new System.Windows.Forms.Button();
+            this.formatItalic = new System.Windows.Forms.Button();
+            this.formatUnderline = new System.Windows.Forms.Button();
+            this.insertSymbolC = new System.Windows.Forms.Button();
+            this.insertSymbolR = new System.Windows.Forms.Button();
+            this.insertSymbolTM = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbFontSize = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // rtfBox
@@ -50,9 +59,10 @@
             this.rtfBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.rtfBox.Location = new System.Drawing.Point(21, 49);
+            this.rtfBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtfBox.Location = new System.Drawing.Point(21, 79);
             this.rtfBox.Name = "rtfBox";
-            this.rtfBox.Size = new System.Drawing.Size(398, 419);
+            this.rtfBox.Size = new System.Drawing.Size(398, 389);
             this.rtfBox.TabIndex = 0;
             this.rtfBox.Text = "";
             this.rtfBox.TextChanged += new System.EventHandler(this.rtfBox_TextChanged);
@@ -107,28 +117,16 @@
             this.lblVersion.TextAlign = System.Drawing.ContentAlignment.TopRight;
             this.lblVersion.Click += new System.EventHandler(this.lblVersion_Click);
             // 
-            // textSpecials
-            // 
-            this.textSpecials.BackColor = System.Drawing.SystemColors.Control;
-            this.textSpecials.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textSpecials.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textSpecials.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.textSpecials.Location = new System.Drawing.Point(153, 524);
-            this.textSpecials.Name = "textSpecials";
-            this.textSpecials.Size = new System.Drawing.Size(74, 19);
-            this.textSpecials.TabIndex = 6;
-            this.textSpecials.Text = "...";
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Gray;
-            this.label4.Location = new System.Drawing.Point(18, 524);
+            this.label4.Location = new System.Drawing.Point(20, 477);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(131, 17);
+            this.label4.Size = new System.Drawing.Size(47, 17);
             this.label4.TabIndex = 7;
-            this.label4.Text = "Special Characters:";
+            this.label4.Text = "Insert:";
             // 
             // button1
             // 
@@ -182,7 +180,7 @@
             // button5
             // 
             this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.button5.Location = new System.Drawing.Point(21, 474);
+            this.button5.Location = new System.Drawing.Point(215, 474);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(99, 23);
             this.button5.TabIndex = 12;
@@ -190,23 +188,143 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(368, 50);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(51, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Super";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // formatBold
+            // 
+            this.formatBold.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.formatBold.Location = new System.Drawing.Point(300, 50);
+            this.formatBold.Name = "formatBold";
+            this.formatBold.Size = new System.Drawing.Size(22, 23);
+            this.formatBold.TabIndex = 14;
+            this.formatBold.Text = "B";
+            this.formatBold.UseVisualStyleBackColor = true;
+            this.formatBold.Click += new System.EventHandler(this.formatBold_Click);
+            // 
+            // formatItalic
+            // 
+            this.formatItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.formatItalic.Location = new System.Drawing.Point(323, 50);
+            this.formatItalic.Name = "formatItalic";
+            this.formatItalic.Size = new System.Drawing.Size(20, 23);
+            this.formatItalic.TabIndex = 15;
+            this.formatItalic.Text = "I";
+            this.formatItalic.UseVisualStyleBackColor = true;
+            this.formatItalic.Click += new System.EventHandler(this.formatItalic_Click);
+            // 
+            // formatUnderline
+            // 
+            this.formatUnderline.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.formatUnderline.Location = new System.Drawing.Point(344, 50);
+            this.formatUnderline.Name = "formatUnderline";
+            this.formatUnderline.Size = new System.Drawing.Size(22, 23);
+            this.formatUnderline.TabIndex = 16;
+            this.formatUnderline.Text = "U";
+            this.formatUnderline.UseVisualStyleBackColor = true;
+            this.formatUnderline.Click += new System.EventHandler(this.formatUnderline_Click);
+            // 
+            // insertSymbolC
+            // 
+            this.insertSymbolC.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertSymbolC.Location = new System.Drawing.Point(66, 474);
+            this.insertSymbolC.Name = "insertSymbolC";
+            this.insertSymbolC.Size = new System.Drawing.Size(27, 23);
+            this.insertSymbolC.TabIndex = 17;
+            this.insertSymbolC.Text = "©®™";
+            this.insertSymbolC.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.insertSymbolC.UseVisualStyleBackColor = true;
+            this.insertSymbolC.Click += new System.EventHandler(this.insertSymbolC_Click);
+            // 
+            // insertSymbolR
+            // 
+            this.insertSymbolR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertSymbolR.Location = new System.Drawing.Point(126, 474);
+            this.insertSymbolR.Name = "insertSymbolR";
+            this.insertSymbolR.Size = new System.Drawing.Size(26, 23);
+            this.insertSymbolR.TabIndex = 18;
+            this.insertSymbolR.Text = "®";
+            this.insertSymbolR.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.insertSymbolR.UseVisualStyleBackColor = true;
+            this.insertSymbolR.Click += new System.EventHandler(this.insertSymbolR_Click);
+            // 
+            // insertSymbolTM
+            // 
+            this.insertSymbolTM.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertSymbolTM.Location = new System.Drawing.Point(96, 474);
+            this.insertSymbolTM.Name = "insertSymbolTM";
+            this.insertSymbolTM.Size = new System.Drawing.Size(26, 23);
+            this.insertSymbolTM.TabIndex = 19;
+            this.insertSymbolTM.Text = "™";
+            this.insertSymbolTM.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.insertSymbolTM.UseVisualStyleBackColor = true;
+            this.insertSymbolTM.Click += new System.EventHandler(this.insertSymbolTM_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(203, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Font Size:";
+            // 
+            // tbFontSize
+            // 
+            this.tbFontSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFontSize.Location = new System.Drawing.Point(259, 50);
+            this.tbFontSize.Name = "tbFontSize";
+            this.tbFontSize.Size = new System.Drawing.Size(36, 24);
+            this.tbFontSize.TabIndex = 22;
+            this.tbFontSize.Text = "12";
+            this.tbFontSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbFontSize.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Location = new System.Drawing.Point(21, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(398, 40);
+            this.panel1.TabIndex = 23;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.tbFontSize);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.insertSymbolTM);
+            this.Controls.Add(this.insertSymbolR);
+            this.Controls.Add(this.insertSymbolC);
+            this.Controls.Add(this.formatUnderline);
+            this.Controls.Add(this.formatItalic);
+            this.Controls.Add(this.formatBold);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textSpecials);
             this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.rtfBox);
+            this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "RTF Markup Helper";
@@ -223,7 +341,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblVersion;
-        private System.Windows.Forms.TextBox textSpecials;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
@@ -231,6 +348,16 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button formatBold;
+        private System.Windows.Forms.Button formatItalic;
+        private System.Windows.Forms.Button formatUnderline;
+        private System.Windows.Forms.Button insertSymbolC;
+        private System.Windows.Forms.Button insertSymbolR;
+        private System.Windows.Forms.Button insertSymbolTM;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbFontSize;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- RTFMarkupHelper/Form1.cs
+++ RTFMarkupHelper/Form1.cs
@@ -10,90 +10,227 @@ using System.Windows.Forms;
 
 namespace RTFMarkupHelper
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-            
-            string w_file = "RTFMarkupHelper.exe";
-            string w_directory = System.IO.Directory.GetCurrentDirectory();
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+						
+			string w_file = "RTFMarkupHelper.exe";
+			string w_directory = System.IO.Directory.GetCurrentDirectory();
 
-            DateTime c3 = System.IO.File.GetLastWriteTime(System.IO.Path.Combine(w_directory, w_file));            
-            lblVersion.Text = "RTF Markup Helper | Build: " + c3.ToString("yyyyMMdd") + "." + c3.ToString("HHmmss");
+			DateTime c3 = System.IO.File.GetLastWriteTime(System.IO.Path.Combine(w_directory, w_file));            
+			lblVersion.Text = "RTF Markup Helper | Build: " + c3.ToString("yyyyMMdd") + "." + c3.ToString("HHmmss");
 
-            textSpecials.Text = "® © ™";
-        }
+		}
 
-        private void rtfBox_TextChanged(object sender, EventArgs e)
-        {
-            textBox.Text = rtfBox.Rtf.ToString();
-        }
+		private void rtfBox_TextChanged(object sender, EventArgs e)
+		{
+			string rtfString = rtfBox.Rtf.ToString();
+			textBox.Text = rtfString; // DecodeEscapedCharacters(ConvertToRtf(rtfString));  //ConvertToRtf(rtfString); // rtfBox.Rtf.ToString();            
+			//GetRtfUnicodeEscapedString(rtfBox.Rtf.ToString());
+		   //  string myText = rtfBox.Rtf. File.ReadAllText(myFilePath, Encoding.Unicode);
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
-        }
+		}
 
-        private void label2_Click(object sender, EventArgs e)
-        {
+		public static string GetRtfUnicodeEscapedString(string s)
+		{
+			var sb = new StringBuilder();
+			foreach (var c in s)
+			{
+				if (c <= 0x7f)
+					sb.Append(c);
+				else
+					// sb.Append("\\u" + Convert.ToUInt32(c) + "?");
+					sb.Append(Convert.ToChar(c));
+			}
+			return sb.ToString();
+		}
 
-        }
+		public static string ConvertToRtf(string rtfText)
+		{
+			int findUTF = -1;
+			bool continueUTFSearch = true;
+			string esacpeddString = "";
 
-        private void lblVersion_Click(object sender, EventArgs e)
-        {
+			do
+			{
+				findUTF = rtfText.IndexOf(@"\'", findUTF + 1);
+				if (findUTF != -1)
+				{
+					string replacedString = rtfText.Substring(findUTF, 4);
+					esacpeddString = replacedString.Substring(2);
 
-        }
+					int esacpeddCharValue = Convert.ToInt16(esacpeddString, 16);
+					char esacpeddChar = Convert.ToChar(esacpeddCharValue);
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Clipboard.SetText(textBox.Text);
-        }
+					esacpeddString = esacpeddChar.ToString();
 
-        private void button2_Click(object sender, EventArgs e)
-        {            
-            DataFormats.Format myFormat = DataFormats.GetFormat(DataFormats.Html);
+					rtfText = rtfText.Replace(replacedString, esacpeddString);
+					findUTF = -1;
+				}
+				else
+				{
+					continueUTFSearch = false;
+				}
+			} while (continueUTFSearch);
 
-            if (rtfBox.CanPaste(myFormat))
-            {
-                rtfBox.Paste(myFormat);         
-            }
-        }
+			return rtfText;
 
-        private void textBox_TextChanged(object sender, EventArgs e)
-        {
+		}	
 
-        }
+	
+	
+		public static string DecodeEscapedCharacters(string escapedCharString){
+		
+				// string s = "具有靜電產生裝置之影像輸入裝置"; // Unicode
+				Encoding Windows1252 = Encoding.GetEncoding("Windows-1252");
+				Encoding Utf8 = Encoding.UTF8;
+				byte[] utf8Bytes = Utf8.GetBytes(escapedCharString); // Unicode -> UTF-8
+				//string badDecode = Windows1252.GetString(utf8Bytes); // Mis-decode as Latin1
+				Console.WriteLine("Escaped: " + escapedCharString);  // Shows your garbage string.
+				string goodDecode = Utf8.GetString(utf8Bytes); // Correctly decode as UTF-8
+				Console.WriteLine("Decoded: " + goodDecode);
 
-        private void button2_Click_1(object sender, EventArgs e)
-        {
-            rtfBox.Rtf = textBox.Text;
-        }
+				// Recovering from bad decode...
+				byte[] originalBytes = Windows1252.GetBytes(escapedCharString);
+				goodDecode = Utf8.GetString(originalBytes);
+				Console.WriteLine("Confirm Decode: " + goodDecode);
+		
+			return goodDecode;
+		
+		}	
+		
+		private void label1_Click(object sender, EventArgs e)
+		{
 
-        private void button3_Click(object sender, EventArgs e)
-        {
+		}
 
-        }
+		private void label2_Click(object sender, EventArgs e)
+		{
 
-        private void button3_Click_1(object sender, EventArgs e)
-        {
-            MessageBox.Show("RTF Markup Helper is a simple C# app to make it easier to view, edit and convert RTF markup code.");
-        }
+		}
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            // default/example RTF 
-            rtfBox.Rtf = @"{\rtf1\ansi\ansicpg1252\deff0{\fonttbl{\f0\fswiss\fprq2\fcharset0 Franklin Gothic Book;}{\f1\froman\fprq2\fcharset0 Times New Roman;}}\viewkind4\uc1\pard\sl276\slmult1\lang1033\b\f0\fs22 Header 1\f1\par\b0\f0 This is a paragraph of text. This is a paragraph of text.This is a paragraph of text. This is a paragraph of text. This is a paragraph of text. This is a paragraph of text. This is a paragraph of text. This is a paragraph of text.\par\par\b Cabecera 2\par\b0 es un p\'e1rrafo de texto. Este es un p\'e1rrafo de text.This es un p\'e1rrafo de texto. Este es un p\'e1rrafo de texto. Este es un p\'e1rrafo de texto. Este es un p\'e1rrafo de texto. Este es un p\'e1rrafo de texto. Este es un p\'e1rrafo de texto.\par}";
-        }
+		private void lblVersion_Click(object sender, EventArgs e)
+		{
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-            textBox.Text = rtfBox.Text = "";
-        }
+		}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Clipboard.SetText(textBox.Text);
+		}
 
-        }
-    }
+		private void button2_Click(object sender, EventArgs e)
+		{            
+			DataFormats.Format myFormat = DataFormats.GetFormat(DataFormats.Html);
+
+			if (rtfBox.CanPaste(myFormat))
+			{
+				rtfBox.Paste(myFormat);         
+			}
+		}
+
+		private void textBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click_1(object sender, EventArgs e)
+		{
+			rtfBox.Rtf = textBox.Text;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button3_Click_1(object sender, EventArgs e)
+		{
+			MessageBox.Show("RTF Markup Helper is a simple C# app to make it easier to view, edit and convert RTF markup code.");
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			// default/example RTF 
+			rtfBox.Rtf = @"{\rtf1\ansi\ansicpg1252\deff0{\fonttbl{\f0\fswiss\fprq2\fcharset0 Franklin Gothic Book;}{\f1\froman\fprq2\fcharset0 Times New Roman;}}\viewkind4\uc1\pard\sl276\slmult1\lang1033\b\f0\fs22 Header 1\f1\par\b0\f0 This is a paragraph of text. This is a paragraph of text.This is a paragraph of text. This is a paragraph of text. This is a paragraph of text. This is a paragraph of text. This is a paragraph of text. This is a paragraph of text.\par\par\b Cabecera 2\par\b0 es un p\'e1rrafo de texto. Este es un p\'e1rrafo de text.This es un p\'e1rrafo de texto. Este es un p\'e1rrafo de texto. Este es un p\'e1rrafo de texto. Este es un p\'e1rrafo de texto. Este es un p\'e1rrafo de texto. Este es un p\'e1rrafo de texto.\par}";
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			textBox.Text = rtfBox.Text = "";
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			if (rtfBox.SelectionCharOffset > 0)
+			{
+				rtfBox.SelectionCharOffset = 0;
+				rtfBox.SelectionFont = new Font(rtfBox.SelectionFont.FontFamily, rtfBox.SelectionFont.Size + 4);
+			}
+			else
+			{
+				rtfBox.SelectionCharOffset = 4;
+				rtfBox.SelectionFont = new Font(rtfBox.SelectionFont.FontFamily, rtfBox.SelectionFont.Size - 4);
+			}
+			
+		}
+
+		private void formatBold_Click(object sender, EventArgs e)
+		{
+			rtfBox.SelectionFont = new Font(rtfBox.Font,
+				rtfBox.SelectionFont.Style ^ FontStyle.Bold);
+		}
+
+		private void formatItalic_Click(object sender, EventArgs e)
+		{
+			rtfBox.SelectionFont = new Font(rtfBox.Font,
+				rtfBox.SelectionFont.Style ^ FontStyle.Italic);
+		}
+
+		private void formatUnderline_Click(object sender, EventArgs e)
+		{
+			rtfBox.SelectionFont = new Font(rtfBox.Font,
+				rtfBox.SelectionFont.Style ^ FontStyle.Underline);
+		}
+
+		private void insertSymbolC_Click(object sender, EventArgs e)
+		{
+			insertCharacter("©");
+		}
+
+		private void insertCharacter(string insertString)
+		{            
+			var selectionIndex = rtfBox.SelectionStart;
+			rtfBox.Text = rtfBox.Text.Insert(selectionIndex, insertString);
+			rtfBox.SelectionStart = selectionIndex + insertString.Length;
+		}
+
+		private void insertSymbolTM_Click(object sender, EventArgs e)
+		{
+			insertCharacter("™");
+		}
+
+		private void insertSymbolR_Click(object sender, EventArgs e)
+		{
+			insertCharacter("®");
+		}
+
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			string fontSizeString = tbFontSize.Text;
+			if (String.IsNullOrEmpty(fontSizeString)) return;
+			int fontSize = Convert.ToInt16(tbFontSize.Text);
+			if (fontSize <= 0) return;
+			rtfBox.SelectionFont = new Font(rtfBox.SelectionFont.FontFamily, fontSize);
+		}
+	}
 }
